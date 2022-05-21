77d86d22e700d076d7f89cf0c636de7cf6e292c3
timDeHof <this.is.tim.dehof@gmail.com>
2022-05-20 21:37:21 -0400

    created the logic to get user input and do the necessary conversions

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-let numberEl = document.getElementById("number").textContent;
+// let numberEl = document.getElementById("number").onclick();
 let length = document.getElementById("results-conversion-text-length");
 let volume = document.getElementById("results-conversion-text-volume");
 let mass = document.getElementById("results-conversion-text-mass");
-console.log(numberEl);
+// console.log(numberEl);
 
-function convertLength() {
+function convertLength(valNum) {
   let lengthConversionRatio = 0.3048; // the number of feet in a meter
-  let meters2feet = +numberEl / lengthConversionRatio;
-  let feet2meters = +numberEl * lengthConversionRatio;
+  let meters2feet = valNum / lengthConversionRatio;
+  let feet2meters = valNum * lengthConversionRatio;
   length.textContent =
-    numberEl +
+    valNum +
     " meters = " +
     meters2feet.toFixed(3) +
     " feet | " +
-    numberEl +
+    valNum +
     " feet = " +
     feet2meters.toFixed(3) +
     " meters";
 }
 
-function convertVolume() {
+function convertVolume(valNum) {
   let volumeConversionRatio = 3.785411784; // the number of liters in a US gallon
-  let liters2Gal = +numberEl / volumeConversionRatio;
-  let gal2Liters = +numberEl * volumeConversionRatio;
+  let liters2Gal = valNum / volumeConversionRatio;
+  let gal2Liters = valNum * volumeConversionRatio;
   volume.textContent =
-    numberEl +
+    valNum +
     " liters = " +
     liters2Gal.toFixed(3) +
     " gallons | " +
-    numberEl +
+    valNum +
     " gallons = " +
     gal2Liters.toFixed(3) +
     " liters";
 }
 
-function convertMass() {
+function convertMass(valNum) {
   let massConversionRatio = 0.45359237; // the number of kilograms in a pound
-  let kilos2pounds = +numberEl / massConversionRatio;
-  let pounds2kilos = +numberEl * massConversionRatio;
+  let kilos2pounds = valNum / massConversionRatio;
+  let pounds2kilos = valNum * massConversionRatio;
   mass.textContent =
-    numberEl +
+    valNum +
     " kilos = " +
     kilos2pounds.toFixed(3) +
     " pounds | " +
-    numberEl +
+    valNum +
     " pounds = " +
     pounds2kilos.toFixed(3) +
     " liters";
 }
-
-convertLength();
-convertVolume();
-convertMass();
+function convert() {
+  valNum = document.getElementById("number").value;
+  convertLength(valNum);
+  convertVolume(valNum);
+  convertMass(valNum);
+}
